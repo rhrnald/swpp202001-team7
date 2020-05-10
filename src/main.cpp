19949bfd7857088b041838e836d2e0b6685046d7
ChaewonKim <chaewon0831@gmail.com>
2020-05-10 18:28:30 +0900

    Seperate modules with main function

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 
 //Our Optimization
 #include "../optimizations/Wrapper.h"
+//#include "../optimizations/PackRegisters.cpp"
 
 //Additional Pass
 #include "llvm/Transforms/InstCombine/InstCombine.h"
@@ -56,87 +57,6 @@ static unique_ptr<Module> openInputFile(LLVMContext &Context,
   return M;
 }
 
-class RemoveUnsupportedOps : public llvm::PassInfoMixin<RemoveUnsupportedOps> {
-  Constant *getZero(Type *T) {
-    return T->isPointerTy() ?
-        ConstantPointerNull::get(dyn_cast<PointerType>(T)) :
-        ConstantInt::get(T, 0);
-  }
-
-public:
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    vector<Instruction *> V;
-    for (auto I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-      Instruction *II = &*I;
-      bool Deleted = false;
-      if (auto *Intr = dyn_cast<IntrinsicInst>(II)) {
-        if (Intr->getIntrinsicID() == Intrinsic::lifetime_start ||
-            Intr->getIntrinsicID() == Intrinsic::lifetime_end) {
-          V.push_back(Intr);
-          Deleted = true;
-        }
-      } else if (auto *UI = dyn_cast<UnreachableInst>(II)) {
-        LLVMContext &Cxt = F.getParent()->getContext();
-        auto *RE = F.getReturnType()->isVoidTy() ? ReturnInst::Create(Cxt) :
-            ReturnInst::Create(Cxt, getZero(F.getReturnType()));
-        RE->insertBefore(UI);
-        V.push_back(UI);
-        Deleted = true;
-      }
-
-      if (!Deleted) {
-        for (unsigned i = 0; i < II->getNumOperands(); ++i) {
-          Value *V = II->getOperand(i);
-          if (isa<UndefValue>(V)) {
-            auto *T = V->getType();
-            II->setOperand(i, getZero(T));
-          }
-        }
-      }
-    }
-
-    for (auto *I : V)
-      I->eraseFromParent();
-    return PreservedAnalyses::all();
-  }
-};
-
-class CheckConstExpr : public llvm::PassInfoMixin<CheckConstExpr> {
-public:
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM) {
-    for (auto &G : M.globals()) {
-      if (auto *F = dyn_cast<Function>(&G)) {
-        for (auto I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-          Instruction *II = &*I;
-          for (auto &V : I->operands()) {
-            if (isa<ConstantExpr>(V)) {
-              errs() << "ERROR: Constant expressions should not exist!\n";
-              errs() << "\t" << *I << "\n";
-              exit(1);
-            }
-          }
-        }
-      } else if (auto *GV = dyn_cast<GlobalVariable>(&G)) {
-        if (GV->hasInitializer()) {
-          errs() << "ERROR: Global variables should not have initializers!\n";
-          errs() << "\t" << *GV << "\n";
-          exit(1);
-        }
-      }
-    }
-    return PreservedAnalyses::all();
-  }
-};
-class DoNothingPass : public llvm::PassInfoMixin<DoNothingPass> {
-  std::string outputFile;
-
-public:
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    //outs() << "Hi! " << F.getName() << "\n";
-    return PreservedAnalyses::all();
-  }
-};
-
 int main(int argc, char **argv) {
   sys::PrintStackTraceOnErrorSignal(argv[0]);
   PrettyStackTraceProgram X(argc, argv);
@@ -163,27 +83,26 @@ int main(int argc, char **argv) {
   PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
 
 
-  FunctionPassManager FPM;
   // If you want to add a function-level pass, add FPM.addPass(MyPass()) here.
+  FunctionPassManager FPM;
 
   FPM.addPass(SROA());
-  FPM.addPass(RemoveUnsupportedOps());
   FPM.addPass(ADCEPass());
+  FPM.addPass(InstCombinePass());
 
-  //FPM.addPass(InstCombinePass());
-  //FPM.addPass(PackRegisters());
-  //FPM.addPass(WeirdArithmetic());
-
-  ModulePassManager MPM;
-  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));  
-  MPM.addPass(CheckConstExpr());
+  FPM.addPass(RemoveUnsupportedOps());
 
   // If you want to add your module-level pass, add MPM.addPass(MyPass2()) here.
+  ModulePassManager MPM;
+  MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));  
+
+  MPM.addPass(CheckConstExpr());
+  MPM.addPass(PackRegisters());
+  MPM.addPass(WeirdArithmetic());
 
   // Run!
   string ll=optOutputLL;
   string s=optOutput;
-
   if(ll=="" && s=="") s="a.s";
 
   if(ll!="") {
@@ -191,6 +110,7 @@ int main(int argc, char **argv) {
     raw_fd_ostream fout(ll, EC);
     fout << *M;
   }
+
   if(s!="") {
     MPM.addPass(SimpleBackend(s, optPrintDepromotedModule));
     MPM.run(*M, MAM);

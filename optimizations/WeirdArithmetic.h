2ba49cd66deeec9676cba50c097cbcd0fece5e68
Jiyong Kang <kangz12345@gmail.com>
2020-05-13 17:26:10 +0900

    Improved code: header include, ifndef
    removed redundant includes in header file as well,
    and used ifndef to make the header file to be included multiple times.

--- optimizations/WeirdArithmetic.h
+++ optimizations/WeirdArithmetic.h
@@ -1,9 +1,7 @@
-#include "llvm/IR/PassManager.h"
+#ifndef WEIRD_ARITHMETIC
+#define WEIRD_ARITHMETIC
+
 #include "llvm/IR/PatternMatch.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
@@ -14,3 +12,5 @@ class WeirdArithmetic : public PassInfoMixin<WeirdArithmetic> {
 public:
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
 };
+
+#endif

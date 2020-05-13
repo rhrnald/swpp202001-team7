58c28fceb4383e51aacccf4e406424d50d3749f4
lego0901 <lego0901@gmail.com>
2020-05-13 21:21:59 +0900

    Packing Registers Optimization (#16)
    * Stablize project. cpp include -> header include. Fix Makefile Dependency
    
    * main.cpp bug(print result without run) fixed
    
    * PackRegisters: Callee part has been done
    
    Sort arguments with their sizes, and pack them.
    Zext and trunc operators has been used
    
    * PackRegisters: Caller part has been done (Vanilla)
    
    Vanilla version of the optimizer has been done.
    But codes are not clean enough.
    
    * Minor correction in src/main.cpp
    
    * PackRegisters.cpp and .h have been changed
    
    Build error has been resolved. (From PackRegisters)
    
    * Build Error has been resolved.
    
    * Basic code cleaned
    
    No commentation yet.
    
    * Commentation has been done for the code review.
    
    Thank you teammates!
    
    * Add comment on packing algorithm
    
    * LastInstruction <- LastMerge. Variables merged
    
    * Packing type can be variant
    
    * Redundant URem has been eliminated
    
    * Minimized includes
    
    * Comment modified: LastInstruction
    
    Co-authored-by: ChaewonKim <chaewon0831@gmail.com>

--- optimizations/PackRegisters.cpp
+++ optimizations/PackRegisters.cpp
@@ -1,25 +1,14 @@
 #include "PackRegisters.h"
 
-#include "llvm/IR/PassManager.h"
-#include "llvm/IR/PatternMatch.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/PassAnalysisSupport.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
 using namespace std;
 
 
-static const unsigned PACK_MAX_SIZE = 64;
-static const unsigned PACK_VALID_COUNT = 3;
+static const unsigned PACK_MAX_SIZE = 64;   // Max int bit count
+static const unsigned PACK_VALID_COUNT = 3; // Min pack arguments requirement
 
 
 ArgumentPackingInfo::ArgumentPackingInfo(Function &F) {
@@ -29,54 +18,66 @@ ArgumentPackingInfo::ArgumentPackingInfo(Function &F) {
     return;
   }
 
-  unique_ptr<DataLayout> DL(new DataLayout(F.getParent()));
-  vector<pair<unsigned, Argument*>> Args;
+  unique_ptr<DataLayout> DL(new DataLayout(F.getParent())); // getTypeSizeInBits
+  vector<pair<unsigned, Argument*>> Args; // vector of (BitSize, Argument*)
 
   for (auto &A : F.args()) {
     Args.emplace_back(DL->getTypeSizeInBits(A.getType()), &A);
   }
   std::sort(Args.begin(), Args.end());
+  // This reverse direction could be more optimal. I left it.
   // std::reverse(Args.begin(), Args.end());
 
   unsigned ArgCount = F.arg_size();
   unsigned PackedCount = 0, PackedSize = Args[0].first,
            PackedFrom = 0, PackedNow = 0;
   
+  unsigned PackBits[4] = {8, 16, 32, 64};
+  IntegerType* PackTypes[4] = {Type::getInt8Ty(*Context), Type::getInt16Ty(*Context),
+                               Type::getInt32Ty(*Context), Type::getInt64Ty(*Context)};
+  
+  // PackFrom: The starting index of Args
+  // PackNow: The moving index of Args
+  // PackedSize: Args[PackFrom].Size + ... + Args[PackNow].Size
   while (PackedFrom < ArgCount) {
-    while (PackedNow + 1 < Args.size() &&
+    // Pack as much Arguments as possible
+    while (PackedNow + 1 < Args.size() && // lookahead checker
             PackedSize + Args[PackedNow + 1].first <= PACK_MAX_SIZE) {
       PackedSize += Args[++PackedNow].first;
     }
     if (PackedNow + 1 - PackedFrom >= PACK_VALID_COUNT) {
+      // If # packed arguments >= PACK_VALID_COUNT
       for (unsigned i = PackedFrom; i <= PackedNow; i++) {
         WillPack[PackedCount].push_back(Args[i].second);
+      }
+      for (unsigned t = 0; t < 4; t++) {
+        if (PackedSize <= PackBits[t]) {
+          // The type of newly packed argument
+          ArgTy.push_back(PackTypes[t]);
+          break;
+        }
       }
       PackedCount++;
       PackedFrom = PackedNow + 1;
       PackedSize = 0;
     } else {
+      // No profit for packing
+      // The type of newly added argument: same to the old one
+      ArgTy.push_back(Args[PackedFrom].second->getType());
       PackedSize -= Args[PackedFrom].first;
       NotPack[PackedCount++] = Args[PackedFrom++].second;
     }
   }
 
+  // New function's info
   PackedArgCount = PackedCount;
+
+  // Memory cleaner
   Args.clear();
 }
 
-vector<Type*>& ArgumentPackingInfo::getArgTy() {
-  vector<Type*> *PArgTy = new vector<Type*>(), &ArgTy = *PArgTy;
-  for (unsigned i = 0; i < PackedArgCount; i++) {
-    if (NotPack.find(i) != NotPack.end()) {
-      ArgTy.push_back(NotPack[i]->getType());
-    } else {
-      ArgTy.push_back(Type::getInt64Ty(*Context));
-    }
-  }
-  return ArgTy;
-}
-
 void ArgumentPackingInfo::clear() {
+  ArgTy.clear();
   NotPack.clear();
   for (auto &[_, V] : WillPack) V.clear();
   WillPack.clear();
@@ -84,99 +85,109 @@ void ArgumentPackingInfo::clear() {
 
 
 Function* PackRegisters::PackRegistersFromCallee(Function *F) {
-  ArgumentPackingInfo *API = PackInfo[F];
-  vector<Type*> &ArgTy = API->getArgTy();
+  ArgumentPackingInfo *API = PackInfo[F]; // Packing Information about F
+  vector<Type*> &ArgTy = API->ArgTy;
   Module *M = F->getParent();
-  StringRef OrigName = F->getName();
   LLVMContext *Context = API->Context;
   unique_ptr<DataLayout> DL(new DataLayout(M));
   ValueToValueMapTy VMap;
 
+  // I will clone a new function from F.
+  // There is no simpler way to change arguments of a function..
   FunctionType *NewFTy = FunctionType::get(F->getFunctionType()->getReturnType(), ArgTy, false);
-  Function *NewF = Function::Create(NewFTy, Function::ExternalLinkage, "", *M);
+  Function *NewF = Function::Create(NewFTy, Function::ExternalLinkage, "", M);
 
+  // Process for API->NotPack. Just replaceAllUseWith.
+  for (auto &[i, A] : API->NotPack) {
+    A->replaceAllUsesWith(NewF->getArg(i));
+    NewF->getArg(i)->setName(A->getName());
+  }
+
+  // Process for API->WillPack. Create a new block on entry that
+  //   unpack the merged arguments. Uses UDiv->URem->Trunc.
   if (!API->WillPack.empty()) {
     BasicBlock *PrevEntry = &(F->getEntryBlock());
     BasicBlock *NewEntry = BasicBlock::Create(*Context);
+    auto &NewEntryInstList = NewEntry->getInstList(); // For inserting new instructions.
 
     if (PrevEntry->getName() == "") PrevEntry->setName("prev_entry");
     NewEntry->setName("split_reg");
 
     F->getBasicBlockList().push_front(NewEntry);
 
-    map<Value*, Value*> ToBeTrunc;
+    map<Value*, Value*> ToBeTrunc; // ZExted arguments; should be truncated.
 
-    for (auto &[i, P] : API->WillPack) {
-      bool Init = 1;
+    for (auto &[i, Pack] : API->WillPack) {
       Value *NewA = NewF->getArg(i);
-      Value *BeforeDiv;
-      Value *BeforeA;
+      Value *BeforeDivisor;
       Value *BeforeRem;
       unsigned BeforeASize;
+      Type* PackType = ArgTy[i];
 
-      for (auto &A : P) {
-        if (!Init) {
-          Instruction *Rem = BinaryOperator::CreateUDiv(BeforeRem, BeforeDiv, "merge");
-          NewEntry->getInstList().push_back(Rem);
-          BeforeRem = Rem;
+      for (unsigned j = 0; j < Pack.size(); j++) {
+        auto &A = Pack[j];
+        // Sequentially read the packed argument
+        if (j != 0) {
+          // E.x., %merge = udiv i64 %merged, 256
+          Instruction *UDiv = BinaryOperator::CreateUDiv(BeforeRem, BeforeDivisor, "merge");
+          NewEntryInstList.push_back(UDiv);
+          BeforeRem = UDiv;
         } else {
           BeforeRem = NewA;
-          Init = 0;
         }
-        unsigned long long ASize = 1ULL << DL->getTypeSizeInBits(A->getType());
-        Value *Div = ConstantInt::get(Type::getInt64Ty(*Context), ASize);
-        Instruction *Rem = BinaryOperator::CreateURem(BeforeRem, Div, "zext");
-        NewEntry->getInstList().push_back(Rem);
-        ToBeTrunc[A] = Rem;
+        if (j + 1 != Pack.size()) {
+          // E.x., %zext.a = urem i64 %merged, 256
+          unsigned long long ASize = 1ULL << DL->getTypeSizeInBits(A->getType());
+          Value *Divisor = ConstantInt::get(PackType, ASize);
+          Instruction *URem = BinaryOperator::CreateURem(BeforeRem, Divisor, "zext." + A->getName());
+          NewEntryInstList.push_back(URem);
+          ToBeTrunc[A] = URem;
 
-        BeforeDiv = Div;
-        BeforeASize = ASize;
+          BeforeDivisor = Divisor;
+          BeforeASize = ASize;
+        } else {
+          ToBeTrunc[A] = BeforeRem;
+        }
       }
+
+      NewF->getArg(i)->setName("merged");
     }
 
-    for (auto &[A, ZA] : ToBeTrunc) {
-      Instruction *Trunc = new TruncInst(ZA, A->getType(), "trunc");
-      NewEntry->getInstList().push_back(Trunc);
+    for (auto &[A, ZExtA] : ToBeTrunc) {
+      // E.x., %trunc.a = trunc i64 %zext.a to i8
+      Instruction *Trunc = new TruncInst(ZExtA, A->getType(), "trunc." + A->getName());
+      NewEntryInstList.push_back(Trunc);
+      // Then %trunc.a replaces every %a
       A->replaceAllUsesWith(Trunc);
     }
 
+    // E.x., br label %prev_entry
     BranchInst *Br = BranchInst::Create(PrevEntry, NewEntry);
+    ToBeTrunc.clear();
   }
 
+  // Clone everything(except arguments) from the original function
   for (auto &BB : *F) {
     BasicBlock *NewBB = CloneBasicBlock(&BB, VMap, "", NewF);
     VMap[&BB] = NewBB;
   }
   NewF->copyAttributesFrom(F);
-
-  for (auto &[i, A] : API->NotPack) {
-    A->replaceAllUsesWith(NewF->getArg(i));
-    NewF->getArg(i)->setName(A->getName());
-  }
-
-  for (auto &A : NewF->args()) {
-    if (A.getName() == "") {
-      A.setName("merged");
-    }
-  }
-
-  NewF->setName(OrigName);
-
-  ArgTy.clear();
+  NewF->setName(F->getName());
 
   return NewF;
 }
 
 pair<Instruction*, CallInst*> PackRegisters::PackRegistersFromCaller(CallInst *CI, Function *NewF) {
-  Function *F = CI->getCalledFunction();
+  Function *F = CI->getCalledFunction(); // The old callee Function ptr (!= NewF)
   ArgumentPackingInfo *API = PackInfo[F];
   Module *M = NewF->getParent();
   LLVMContext *Context = API->Context;
   BasicBlock *BB = CI->getParent();
   unique_ptr<DataLayout> DL(new DataLayout(M));
+  auto &BBInstList = BB->getInstList(); // For inserting new instructions
 
-  vector<Value*> Args(API->PackedArgCount, NULL);
-  map<Argument*, Value*> ArgToParam;
+  vector<Value*> Args(API->PackedArgCount, NULL); // Arguments for NewF
+  map<Argument*, Value*> ArgToParam; // Argument to Parameters map
 
   for (unsigned i = 0; i < F->arg_size(); i++) {
     Argument *A = F->getArg(i);
@@ -184,57 +195,67 @@ pair<Instruction*, CallInst*> PackRegisters::PackRegistersFromCaller(CallInst *C
     ArgToParam[A] = V;
   }
   
+  // Process NotPack. Just take it
   for (auto &[i, A] : API->NotPack) {
     Args[i] = ArgToParam[A];
   }
 
-  Instruction *LastInstruction = CI;
+  Instruction *LastInstruction = CI; // The last Instruction's ptr
 
+  // Process WillPack. Merge registers before the function call
   for (auto &[i, Pack] : API->WillPack) {
-    vector<Instruction*> ZExts;
+    vector<Instruction*> ZExts; // ZExts of every original argument
+    Type *PackType = API->ArgTy[i];
 
     for (auto &A : Pack) {
+      // E.x., %zext.a = zext i8 %a to i64
       Value *V = ArgToParam[A];
-      Instruction *ZExt = new ZExtInst(V, Type::getInt64Ty(*Context), "zext");
-      BB->getInstList().insertAfter(LastInstruction->getIterator(), ZExt);
+      Instruction *ZExt = new ZExtInst(V, PackType, "zext." + V->getName());
+      BBInstList.insertAfter(LastInstruction->getIterator(), ZExt);
       LastInstruction = ZExt;
 
       ZExts.push_back(ZExt);
     }
 
-    Instruction *LastMerge = LastInstruction;
-
     for (int j = Pack.size()-1; j >= 0; j--) {
+      // Reverse order packing
       if (j + 1 < Pack.size()) {
-        Instruction *Xor = BinaryOperator::CreateXor(LastMerge, ZExts[j], "merge");
-        LastMerge = Xor;
-        BB->getInstList().insertAfter(LastInstruction->getIterator(), Xor);
+        // E.x., %merge.1 = xor i64 %merge, %zext.a
+        Instruction *Xor = BinaryOperator::CreateXor(LastInstruction, ZExts[j], "merge");
+        BBInstList.insertAfter(LastInstruction->getIterator(), Xor);
         LastInstruction = Xor;
       }
       if (j > 0) {
+        // E.x., %merge.1 = mul i64 %merge, 256
         unsigned long long NextSize = 1ULL << DL->getTypeSizeInBits(Pack[j-1]->getType());
-        Value *MulOp2 = ConstantInt::get(Type::getInt64Ty(*Context), NextSize);
-        Instruction *Mul = BinaryOperator::CreateMul(LastMerge, MulOp2, "merge");
-        LastMerge = Mul;
-        BB->getInstList().insertAfter(LastInstruction->getIterator(), Mul);
+        Value *Multiplier = ConstantInt::get(PackType, NextSize);
+        Instruction *Mul = BinaryOperator::CreateMul(LastInstruction, Multiplier, "merge");
+        BBInstList.insertAfter(LastInstruction->getIterator(), Mul);
         LastInstruction = Mul;
       }
     }
 
-    Args[i] = LastMerge;
+    // NewF's ith argument is LastInstruction
+    Args[i] = LastInstruction;
+    ZExts.clear();
   }
 
-  FunctionType *FTy = NewF->getFunctionType();
-  CallInst *NewCI = CallInst::Create(FTy, NewF, Args, "");
+  // Create a new function call and copy every information from the original
+  CallInst *NewCI = CallInst::Create(NewF->getFunctionType(), NewF, Args, "");
   NewCI->setAttributes(CI->getAttributes());
   NewCI->setCallingConv(CI->getCallingConv());
   NewCI->setTailCall(CI->getTailCallKind());
   NewCI->setDebugLoc(CI->getDebugLoc());
 
+  // Set CallInst's target
   if (F->getReturnType() != Type::getVoidTy(*Context)) {
     NewCI->setName(CI->getName());
   }
 
+  Args.clear();
+  ArgToParam.clear();
+
+  // Returns (Insertion position, Newly created caller)
   return make_pair(LastInstruction, NewCI);
 }
 
@@ -242,38 +263,25 @@ PreservedAnalyses PackRegisters::run(Module &M, ModuleAnalysisManager &MAM) {
   FunctionAnalysisManager &FAM = MAM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
   
   vector<Function*> OrigFunctions;
-  vector<CallInst*> OrigCaller;
   map<Function*, Function*> FunctionMap;
+  vector<CallInst*> OrigFunctionCall;
   map<CallInst*, pair<Instruction*, CallInst*>> CallerMap;
 
   for (Function &F : M) {
-    if (F.isDeclaration()) {
-      continue;
-    }
-    PackInfo[&F] = new ArgumentPackingInfo(F);
-    OrigFunctions.push_back(&F);
-
-    for (auto &BB : F) {
-      for (auto &I : BB) {
-        CallInst *CI = dyn_cast<CallInst>(&I);
-        if (!CI) continue;
-
-        Function *Called = CI->getCalledFunction();
-        if (!Called->isDeclaration()) {
-          OrigCaller.push_back(CI);
-        }
-      }
+    if (!F.isDeclaration()) { // Do not optimizes declaration(e.x., malloc)
+      // Make every ArgumentPackingInfo, and push to the vector
+      PackInfo[&F] = new ArgumentPackingInfo(F);
+      OrigFunctions.push_back(&F);
     }
   }
 
   for (Function *F : OrigFunctions) {
+    // Optimizes callees first.
+    // The old function F is not deleted yet. (Or, this causes an error)
+    // But the new function NewF is added to the module. (Or, this causes an error)
     Function *NewF = PackRegistersFromCallee(F);
     FunctionMap[F] = NewF;
   }
-
-  int cnt = 0;
-
-  vector<CallInst*> ToRemove;
 
   for (Function &F : M) {
     for (auto &BB : F) {
@@ -281,29 +289,42 @@ PreservedAnalyses PackRegisters::run(Module &M, ModuleAnalysisManager &MAM) {
         CallInst *CI = dyn_cast<CallInst>(&I);
         if (!CI) continue;
 
+        // Okay it's function call.
         Function *Called = CI->getCalledFunction();
         if (!Called->isDeclaration()) {
+          // Optimizes caller part.
+          // The old function call CI is not deleted yet.
+          // Also, the new function call is not inserted yet.
           CallerMap[CI] = PackRegistersFromCaller(CI, FunctionMap[Called]);
-          ToRemove.push_back(CI);
+          OrigFunctionCall.push_back(CI);
         }
       }
     }
   }
 
-  for (auto &CI : ToRemove) {
+  for (auto &CI : OrigFunctionCall) {
     BasicBlock *BB = CI->getParent();
     auto &[InsertionPoint, NewCI] = CallerMap[CI];
 
+    // Insert the new function call after the Instruction ptr.
+    // And replaceAllUses, and finally delete the original one.
     BB->getInstList().insertAfter(InsertionPoint->getIterator(), NewCI);
     CI->replaceAllUsesWith(NewCI);
     CI->removeFromParent();
   }
 
   for (Function *F : OrigFunctions) {
+    // Remove the original functions and rename the new ones.
     Function *NewF = FunctionMap[F];
     F->removeFromParent();
     NewF->setName(F->getName());
   }
+
+  // Memory cleaner
+  OrigFunctions.clear();
+  FunctionMap.clear();
+  CallerMap.clear();
+  OrigFunctionCall.clear();
 
   return PreservedAnalyses::all();
 }
--- optimizations/PackRegisters.h
+++ optimizations/PackRegisters.h
@@ -1,33 +1,61 @@
-#include "llvm/IR/PassManager.h"
-#include "llvm/IR/PatternMatch.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
+#ifndef PACK_REGISTERS
+#define PACK_REGISTERS
+
+#include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
 using namespace std;
 
 
+/* ArgumentPackingInfo - A class for every Function in LLVM,
+ *   that contains register packing information. The indices for ArgTy,
+ *   NotPack, WillPack are for the newly converted(argument packed)
+ *   function. 
+ * */
 class ArgumentPackingInfo {
 public:
+  /* ArgTy - Argument types of the new function */
+  vector<Type*> ArgTy;
+  /* NotPack - ArgNo to Argument* map.
+   *   For those arguments that will not be packed */
   map<unsigned, Argument*> NotPack;
+  /* WillPack - ArgNo to vector<Argument*> map.
+   *   For those arguments that should be packed, encoded with std::vector */
   map<unsigned, vector<Argument*>> WillPack;
+  /* PackedArgCount - The number of the new function's arguments */
   unsigned PackedArgCount;
+  /* Context - The context of the function */
   LLVMContext *Context;
 
+  /* ArgumentPackingInfo - A constructor of the class.
+   *   It makes the above data-structures for register packing. */
   ArgumentPackingInfo(Function &F);
-  vector<Type*>& getArgTy();
+
+  /* clear - Memory cleaner function */
   void clear();
 };
 
+
+/* PackRegisters - An LLVM optimization class by packing the argument
+ *   of the function. It is divided into callee part(PackRegistersFromCalle)
+ *   and caller part(PackRegistersFromCaller).
+ * */
 class PackRegisters : public PassInfoMixin<PackRegisters> {
 private:
+  /* PackInfo - A map from Function*(old function ptr) to
+   *   ArgumentPackingInfo* */
   map<Function*, ArgumentPackingInfo*> PackInfo;
+  /* PackRegistersFromCallee - Callee update. Returns the newly created
+   *   function. */
   Function* PackRegistersFromCallee(Function *F);
+  /* PackRegistersFromCaller - Caller update. Returns a std::pair of
+   *   insertion ptr of the new CallInst(Instruction ptr) and the
+   *   corresponding new CallInst. */
   pair<Instruction*, CallInst*> PackRegistersFromCaller(CallInst *CI, Function *NewF);
   
 public:
+  /* run - Runs the main optimization! */
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
 };
+#endif

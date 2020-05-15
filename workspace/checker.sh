5f561194aabdc4212f4e79589d9c921985f6f8b4
Jiyong Kang <kangz12345@gmail.com>
2020-05-15 16:18:49 +0900

    Modified checker.sh to take arguments.
    now checker.sh takes a list of testcases. to test all cases,
    just pass testcases/*.
    Makefile is now call workspace/checker.sh workspace/testcases/*.
    also, now checker.sh does not call any cd.

--- workspace/checker.sh
+++ workspace/checker.sh
@@ -1,36 +1,31 @@
 #/bin/bash
 
-PREVWD=$PWD
-cd $(dirname $0)
+DIR=$(dirname $0)
 
 echo
 echo Case test start
 
-python3 log_checker.py
+python3 $DIR/log_checker.py
 
-for tc in testcases/*/; do
-  echo Testing $tc
-  cd $tc
+for tc in $@; do
+  echo Testing $tc/
 
-  orig_ll=src/*.ll
-  orig_s=src/*.s
-  test_s="ours.s"
+  orig_ll=$tc/src/*.ll
+  orig_s=$tc/src/*.s
+  test_s=$tc/"ours.s"
   log="sf-interpreter.log"
-  log1="log1"
-  log2="log2"
+  log1=$tc/"log1"
+  log2=$tc/"log2"
 
-  ../../../bin/sf-compiler-team7 $orig_ll -o $test_s
+  $DIR/../bin/sf-compiler-team7 $orig_ll -o $test_s
 
-  for inp in test/input*; do
-    ../../../bin/interpreter $orig_s < $inp > .tmp1
+  for inp in $tc/test/input*; do
+    $DIR/../bin/interpreter $orig_s < $inp > $tc/.tmp1
     mv $log $log1
-    ../../../bin/interpreter $test_s < $inp > .tmp2
+    $DIR/../bin/interpreter $test_s < $inp > $tc/.tmp2
     mv $log $log2
-    python3 ../../log_checker.py $inp .tmp1 .tmp2 $log1 $log2
-    rm .tmp1 .tmp2 $log1 $log2
+    python3 $DIR/log_checker.py $inp $tc/.tmp1 $tc/.tmp2 $log1 $log2
+    rm $tc/.tmp1 $tc/.tmp2 $log1 $log2
   done
   rm $test_s
-  cd ../..
 done
-
-cd $PREVWD

b40e065ef0ba1a2cbaee404385ccd29e499f77f4
Woosung Song <lego0901@gmail.com>
2020-05-24 01:36:48 +0900

    Post Unroll - Parser Done
    Now is beta

--- src/passes/LoopUnrollPostHelper.cpp
+++ src/passes/LoopUnrollPostHelper.cpp
@@ -24,16 +24,23 @@ PreservedAnalyses LoopUnrollPostHelper::run(Module &M, ModuleAnalysisManager &MA
     if (!isa<Function>(F)) continue;
     FunctionMap[F.getName()] = &F;
   }
-  bool SkipOne = false;
+  bool SkipOne = false, Ended = false;
   string FnName;
   Function *FnParse = NULL;
   vector<pair<vector<Instruction *>, Instruction*>> Replacements;
+  vector<Instruction *> Consecutives;
   CallInst *NewCall = NULL;
   Value *X, *Y;
   vector<Value *> Args;
   unsigned ArgNum = 0;
   for (auto &F : M) for (auto &BB : F) for (auto &I : BB) {
-    if (SkipOne) { SkipOne = false; continue; }
+    if (FnParse) Consecutives.push_back(&I);
+    if (SkipOne) {
+      if (Ended) {
+        Ended = false, FnParse = NULL;
+      }
+      SkipOne = false; continue;
+    }
     if (!FnParse) {
       if ((FnName = getParse(I.getName(), PREFIX_OF_START, SUFFIX)) != "") {
         FnParse = FunctionMap[FnName];
@@ -42,23 +49,28 @@ PreservedAnalyses LoopUnrollPostHelper::run(Module &M, ModuleAnalysisManager &MA
     } else {
       if (ArgNum < FnParse->arg_size()) {
         if (StoreInst *S = dyn_cast<StoreInst>(&I)) {
-
+          X = S->getOperand(0);
         } else if (ZExtInst *Z = dyn_cast<ZExtInst>(&I)) {
-
+          X = Z->getOperand(0);
+          SkipOne = true;
         } else if (PtrToIntInst *P = dyn_cast<PtrToIntInst>(&I)) {
-
+          X = P->getOperand(0);
+          SkipOne = true;
         }
+        Args.push_back(X);
       } else {
-        if (FnParse->getReturnType() != Type::getVoidTy(Context)) {
-          if (PtrToIntInst *P = dyn_cast<PtrToIntInst>(&I)) {
-
-          } else if (IntToPtrInst *P = dyn_cast<IntToPtrInst>(&I)) {
-
+        if (!Ended) {
+          NewCall = CallInst::Create(FnParse->getFunctionType(), FnParse, Args, FnName);
+          if (FnParse->getReturnType() != Type::getVoidTy(Context)) {
+            I.replaceAllUsesWith(NewCall);
+          }
+          vector<Value *> Empty; std::swap(Args, Empty);
+          Ended = true;
+        } else {
+          if ((FnName = getParse(I.getName(), PREFIX_OF_START, SUFFIX)) != "") {
+            SkipOne = true;
           }
         }
-        NewCall = CallInst::Create(FnParse->getFunctionType(), FnParse, Args, FnName);
-        vector<Value *> Empty; std::swap(Args, Empty);
-        SkipOne = true;
       }
     }
   }
